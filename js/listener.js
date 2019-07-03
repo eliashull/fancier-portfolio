b066a72ffc4b7163d21d374f5917c96509f94298
Bryan Hull <bryaneliashull@mbp-baby.uconnect.utah.edu>
2019-07-02 19:18:30 -0600

    Added listener to .js

---
+++ js/listener.js
@@ -0,0 +1,74 @@
+// UI Components (Nav Icons)
+
+/*
+input_ico_locate.addEventListener("click",function(){
+    API_request();
+});
+*/
+
+// UI Component (Buttons)
+
+/*
+
+input_btn_now.addEventListener("click",function(){
+    get_forecast("now");
+});
+input_btn_hourly.addEventListener("click",function(){
+    get_forecast("hourly");
+});
+input_btn_daily.addEventListener("click",function(){
+    get_forecast("daily");
+});
+input_btn_previous.addEventListener("click",function(){
+    get_forecast("previous");
+});
+input_btn_next.addEventListener("click",function(){
+    get_forecast("next");
+});
+input_btn_getforecast.addEventListener("click",function(){
+    API_request();
+});
+input_btn_settings.addEventListener("click",function(){
+    router("view_settings");
+});
+
+*/
+
+/* jQuery Example Event Listener - Similar to plain Javascript example above
+$("#input_ico_locate").on('click',function(){
+  API_request();  
+})
+*/
+
+
+introduction_link.addEventListener("click",function(){
+    console.log("Hello World! introduction_link is doing a thing?");
+});
+
+instructionaldesign_link.addEventListener("click",function(){
+    console.log("Hello World! instructionaldesign_link is doing a thing?");
+});
+
+digitalpublishing_link.addEventListener("click",function(){
+    console.log("Hello World! digitalpublishing_link is doing a thing?");
+});
+
+graphicdesign_link.addEventListener("click",function(){
+    console.log("Hello World! graphicdesign_link is doing a thing?");
+});
+
+videoproduction_link.addEventListener("click",function(){
+    console.log("Hello World! videoproduction_link is doing a thing?");
+});
+
+webdevelopment_link.addEventListener("click",function(){
+    console.log("Hello World! webdevelopment_link is doing a thing?");
+});
+
+curriculumvitae_link.addEventListener("click",function(){
+    console.log("Hello World! curriculumvitae_link is doing a thing?");
+});
+
+contact_link.addEventListener("click",function(){
+    console.log("Hello World! contact_link is doing a thing?");
+});
